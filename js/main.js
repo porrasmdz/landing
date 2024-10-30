c1f46f3c1aecc0f6e9b0ec0812e6b25440c3d230
Cliente CONTROL-PC <CltControl@GGIECLUGWRK165.espol.edu.ec>
2024-10-30 09:49:06 -0500

    feat: suscriber count per day

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const databaseURL = ""
+const databaseURL = "https://landing-f6015-default-rtdb.firebaseio.com/data.json"
 let sendData = () => {
   // Obtén los datos del formulario
   const formData = new FormData(form); //form es el id del elemento del form de donde quiero sacar los datos
@@ -20,13 +20,65 @@ let sendData = () => {
     .then(result => {
       alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
       form.reset()
+      getData()
     })
     .catch(error => {
       alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
     });
 }
+
 let ready = () => {
   console.log('DOM está listo')
+  getData();
+}
+
+let getData = async () => {
+  try {
+    const response = await fetch(databaseURL);
+    if (!response.ok) {
+      alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }
+    const data = await response.json();
+    if (data != null) {
+
+      // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+      let countSuscribers = new Map()
+
+      if (Object.keys(data).length > 0) {
+        for (let key in data) {
+
+          let { email, saved } = data[key]
+
+          let date = saved.split(",")[0]
+
+          let count = countSuscribers.get(date) || 0;
+          countSuscribers.set(date, count + 1)
+        }
+      }
+      // END
+      // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+      if (countSuscribers.size > 0) {
+
+        subscribers.innerHTML = ''
+
+        for (let [date, count] of countSuscribers) {
+          let rowTemplate = `
+                  <tr>
+                      <th scope="row">1</th>
+                      <td>${date}</td>
+                      <td>${count}</td>
+                  </tr>`
+          subscribers.innerHTML += rowTemplate
+        }
+      }
+      // END
+
+    }
+
+  } catch (error) {
+    // Muestra cualquier error que ocurra durante la petición
+    alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+  }
 }
 
 let loaded = () => {
